d04c1766523d0f8896dffc36b8847db5581cabb7
Kometha <kenethcubas2002@unitec.edu>
2025-06-18 00:01:24 -0600

    Se añade el componente InventoryAlertsComponent, que permite gestionar y mostrar alertas de inventario. Este nuevo componente mejora la modularidad del código y contribuye a la organización del dashboard.

---
+++ src/app/dashboard/inventory-alerts/inventory-alerts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-alerts',
+  imports: [],
+  templateUrl: './inventory-alerts.component.html',
+  styleUrl: './inventory-alerts.component.scss'
+})
+export class InventoryAlertsComponent {
+
+}
